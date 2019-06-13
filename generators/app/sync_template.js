ca861406c6e413f364c1e32ada72bcee3fa7534f
Pratama Nur Wijaya <set.mnemonix@gmail.com>
2019-06-13 17:22:12 +0700

    remove unnecesary file

--- generators/app/sync_template.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const rimraf = require('rimraf');
-const replace = require('replace');
-const ncp = require('ncp').ncp;
-const nodegit = require('nodegit');
-const clone = nodegit.Clone;
-const mv = require('mv');
-
-const {replaceConfig, createReplacement} = require('./config/replace');
